f574648d483d2c9affe92fdf9575cb0164b787c9
LaziukAlena <Lazyuk.elena1988@gmail.com>
2025-10-21 17:21:34 +0300

    урок 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 new WOW().init();
+
+const mySwiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+
+    },
+});
+
